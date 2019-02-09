60edcf055d32926990bfdc0b1ac05f0db54ef566
Muhammad Dadu <muhammad@dadu.co>
2019-02-09 12:34:21 +0000

    updated docs and removed gruntfile based tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,15 +6,6 @@
 
 module.exports = function(grunt) {
     grunt.initConfig({
-        mochaTest: {
-            test: {
-                src: ['test/*_test.js']
-			}
-			watch: {
-				src: ['test/*_test.js'],
-				watch: true,
-			}
-        },
 	    jsdoc : {
 	        dist : {
 				src: ['bin/**/*.js', 'hooks/**/*.js', 'hookslib/**/*.js', 'lib/**/*.js', 'test/lib.js', 'index.js', 'README.md'],
@@ -27,12 +18,7 @@ module.exports = function(grunt) {
 	    }
     });
 
-    grunt.loadNpmTasks('grunt-mocha-test');
     grunt.loadNpmTasks('grunt-jsdoc');
-
-	grunt.registerTask('test', 'mochaTest');
-	grunt.registerTask('test:watch', 'mochaTest:watch');
-	grunt.registerTask('gendoc', ['mochaTest', 'jsdoc']);
-
-	grunt.registerTask('defualt', ['mochaTest', 'jsdoc']);
+	grunt.registerTask('gendoc', ['jsdoc']);
+	grunt.registerTask('defualt', ['jsdoc']);
 };
